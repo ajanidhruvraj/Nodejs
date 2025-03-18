17510fdcdbdca771feddef50f093a1f35ec964b3
Dhruvraj ajani <166250079+ajanidhruvraj@users.noreply.github.com>
2025-03-18 10:55:13 +0000

    node2

---
+++ math.js
@@ -0,0 +1,15 @@
+//  function add(a,b){
+//     return a+b
+//  }
+
+//  function sub(a,b){
+//     return a-b
+//  }
+
+//  module.exports={
+//     add,
+//     sub
+//  }
+
+exports.add=(a,b)=> a+b;// anonymous function because it does not have name
+exports.sub=(a,b)=> a-b;
--- node.js
+++ node.js
@@ -1,1 +1,7 @@
+const math = require("./math")
+//const {add,sub} = require("./math") we can use this also
 console.log("hello");
+
+console.log(math)
+console.log(math.add(2,5));
+
